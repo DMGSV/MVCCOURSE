768732925858d4d0ed61b36d951cc72f2d37eee2
Dmitry Gusev <gadosti26@gmail.com>
2021-02-09 21:33:19 +0200

    CarRepository enabled

--- HundaiService/HundaiService/CarRepository.cs
+++ HundaiService/HundaiService/CarRepository.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using HundaiService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace HundaiService
 {
     public class CarRepository
     {
+        private static List<Car> _cars;
+
+        public CarRepository()
+        {
+            _cars = new List<Car> { new Car { IDCar = 1, Brand = "Hundai", Model = "S", BodyModel = "SEDAN" } };
+        }
+
+        public async Task<List<Car>> GetCars()
+        {
+            return await Task.Run(() => _cars);
+        }
+
+        public async Task<Car> GetCar(int id)
+        {
+            return await Task.Run(() => _cars.FirstOrDefault(f => f.IDCar == id));
+        }
     }
 }
--- HundaiService/HundaiService/Models/Car.cs
+++ HundaiService/HundaiService/Models/Car.cs
@@ -8,6 +8,7 @@ namespace HundaiService.Models
 {
     public class Car
     {
+        public int IDCar { get; set; }
         public string Brand { get; set; }
         [StringLength(15, MinimumLength = 1, ErrorMessage = "Must be max 15 symbols")]
         public string Model { get; set; }

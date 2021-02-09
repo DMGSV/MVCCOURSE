2562ab89b780f53155d6e5cfddb6bfab7881a172
Dmitry Gusev <gadosti26@gmail.com>
2021-02-09 20:12:06 +0200

    first commit

---
+++ HundaiService/HundaiService/Controllers/CarController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HundaiService.Controllers
+{
+    public class CarController : Controller
+    {
+        public CarController()
+        {
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddCar()
+        {
+            return View();
+        }
+        public ActionResult EditCar()
+        {
+            return View();
+        }
+        public ActionResult DeleteCar()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HundaiService/HundaiService/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HundaiService.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "Must be max 15 symbols")]
+        public string Model { get; set; }
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "Must be max 15 symbols")]
+        public string BodyModel { get; set; }
+    }
+}
